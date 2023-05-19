5bcf814672c8a046764655d0a9f254cd9fe809ae
postre554 <moglimon554@gmail.com>
2023-05-19 18:02:32 -0400

    segundo - inclusion de helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home',              link_to:"pages#home"
-  get 'pages/projects',          link_to:"pages#projects"
-  get 'pages/contact',           link_to:"pages#contact"
+  get 'pages/home',              link_to:"pages#home",      as:"jome"
+  get 'pages/projects',          link_to:"pages#projects",      as:"proys"
+  get 'pages/contact',           link_to:"pages#contact",       as:"contacto_si"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
